34f354224c72b9ad61e40c20efcce6662e0f6b6a
Ibrahim AlKhayat <ibrahim.alkhayat@tum.de>
2015-12-06 18:27:50 +0100

    added time picker

--- app/src/main/java/de/tum/citywatch/OffersFragment.java
+++ app/src/main/java/de/tum/citywatch/OffersFragment.java
@@ -1,10 +1,17 @@
 package de.tum.citywatch;
 
+import android.app.DialogFragment;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Ibrahim on 30/11/2015.
@@ -13,11 +20,29 @@ public class OffersFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // inflat and return the layout
         View view = inflater.inflate(R.layout.fragment_offers, container,
                 false);
 
-        // Perform any camera updates here
+        Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
+        List<String> list = new ArrayList<String>();
+        list.add("small");
+        list.add("normal");
+        list.add("big");
+        list.add("really big");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(view.getContext(),
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+
+        Button timeButton = (Button) view.findViewById(R.id.timeButton);
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment newFragment = new TimePickerFragment();
+                newFragment.show(getFragmentManager(), "timePicker");
+            }
+        });
+
         return view;
     }
 }
